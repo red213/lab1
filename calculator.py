3be5b93056361830e24029b31b384c9d261200b0
red213 <red.112141@gmail.com>
2019-10-29 01:46:35 +0300

    calculator

---
+++ calculator.py
@@ -0,0 +1,56 @@
+class Check(object):
+    def __init__(self, a, b):
+        self.num1 = a
+        self.num2 = b
+
+    def check(self):
+        try:
+            int(self.num1) and int(self.num2)
+            return True
+        except ValueError:
+            return False
+
+
+class Calculator(object):
+    def __init__(self, num1, num2):
+        self.num1 = num1
+        self.num2 = num2
+
+    def add(self, num1, num2):
+        return num1 + num2
+
+    def subtract(self, num1, num2):
+        return num1 - num2
+
+    def multiply(self, num1, num2):
+        return num1 * num2
+
+    def devide(self, num1, num2):
+        return num1 / num2
+
+    def power(self, num1, num2):
+        return num1 ** num2
+
+    def remainder(self, num1, num2):
+        return num1 // num2
+
+
+
+class Operations(Calculator):
+    def __init__(self, num1, num2):
+        super().__init__(num1, num2)
+
+    def choice(self):
+        operation = str(input("Print operation: "))
+        switcher = {'+': self.add(self.num1, self.num2),
+                    '-': self.subtract(self.num1, self.num2),
+                    '*': self.multiply(self.num1, self.num2),
+                    '/': self.devide(self.num1, self.num2),
+                    '**': self.power(self.num1, self.num2),
+                    '//': self.remainder(self.num1, self.num2)}
+        result = switcher.get(operation)
+        return result
+
+
+
+
